c53c9ec081da76e6d24c7ef9a3967f281db1d4a0
lee <lee@DESKTOP-H5157CI>
2024-02-13 17:43:20 +0900

    초기설정 커밋

---
+++ SpringJpaData/src/test/java/com/study/jpa/data/SpringJpaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jpa.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
